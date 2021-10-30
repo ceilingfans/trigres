73a65e3e1bdd8949650b3201a49b270fde981911
awned <65061212+ceilingfans@users.noreply.github.com>
2021-10-31 01:32:15 +0800

    Delete lib directory

--- lib/util/trigo.hpp
+++
@@ -1,5 +0,0 @@
-#include <string>
-
-void resolve_sine(const int& opp, const int& hyp, const int& quad, const std::string& name = "x");
-void resolve_cosine(const int& adj, const int& hyp, const int& quad, const std::string& name = "x");
-void resolve_tangent(const int& opp, const int& adj, const int& quad, const std::string& name = "x");
--- lib/util/util.hpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include <vector>
-
-std::vector<std::string> split_text(std::string s, const std::string& delimiter);
-std::string pchar_to_string(char* s);
-std::string normalize_fraction(float numerator, float denominator);
-
-void print_equations(const float& hyp, const float& opp, const float& adj, const std::string& name = "x");
